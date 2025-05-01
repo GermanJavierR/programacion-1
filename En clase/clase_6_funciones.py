5df1da604991c17c8d86e570d190c820e876f31a
GermanJavierR <german.j.r1002@gmail.com>
2025-05-01 17:37:47 -0300

    subiendo los ejercicios propuestos en le videos de la clase 6

---
+++ En clase/clase_6_funciones.py
@@ -0,0 +1,16 @@
+#Ejercicios propuestos en le video.
+
+def chau_nombre(nombre):
+    print("Chau,", nombre)
+
+chau_nombre("Germán")
+
+
+def multiplicar(a, b):
+    resultado = a * b
+    return resultado
+
+multiplicación = multiplicar(3, 5)
+print(f"La multiplicación es: {multiplicación}")
+
+
