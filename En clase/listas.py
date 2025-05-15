1bcad972f26fea0ea31bedb4a1a343f815442a65
GermanJavierR <german.j.r1002@gmail.com>
2025-05-14 21:29:34 -0300

    subiendo archivo sobre listas

---
+++ En clase/listas.py
@@ -0,0 +1,47 @@
+calificaciones = [98, 88, 54, 100, 87, 68]
+
+suma = 0
+
+for i in range(len(calificaciones)):
+    suma += calificaciones[i]
+
+promedio = suma / len(calificaciones)
+
+# print(promedio)
+
+
+ceros = [0] * 5
+
+for i in range(len(ceros)):
+    ceros
+
+print(ceros)
+
+
+mi_lista = [10, "Hola", True, 1.4]
+
+print(mi_lista[0])
+
+print(mi_lista[1])
+
+print(mi_lista[-1])
+
+
+mi_lista = [4, 9, 3, 7, 1]
+
+print(mi_lista)
+
+
+for i in range(len(mi_lista)):
+    print(mi_lista[i])
+
+
+
+mi_lista2 = [1, 2, 3, 4, 5]
+
+total = 0
+
+for i in range(len(mi_lista2)):
+    total += mi_lista2[i]
+
+print(total)
